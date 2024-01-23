4cff34e1773cb7dac3deea01cbdf564ed15f6002
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-01-22 20:45:45 -0500

    Messing around with progress bars and more settings

--- LearningTopics/LearningSpectre/learningSpectreTerm/Program.cs
+++ LearningTopics/LearningSpectre/learningSpectreTerm/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Spectre.Console;
 
-AnsiConsole.Write(new FigletText("Learning Spectre\n\n").Color(Color.Green));
+AnsiConsole.Write(new FigletText("Learning Spectre\n\n").Color(Color.Red).Centered());
 
 
 
-AnsiConsole.Write("\n\nFirst up we have..\n");
+AnsiConsole.MarkupLine("\n[bold]First things first.. Prompts..[/]");
 var prompttitle = new Rule("[red]PROMPTS AND ASK[/]");
 prompttitle.Justification = Justify.Center;
 AnsiConsole.Write(prompttitle);
@@ -23,19 +23,38 @@ var password = AnsiConsole.Prompt(new TextPrompt<string>("Enter your [red]Passwo
     .PromptStyle("red")
     .Secret());
 Console.ForegroundColor= ConsoleColor.DarkGreen;
+if (name != "Brandon")
+{
 Console.WriteLine($"\nUsername: {name}\nPassword: {password}");
 Console.WriteLine();
 Console.WriteLine();
+}
+if (name =="Brandon")
+{
+    Console.WriteLine("\nNah Because you really thought you were on some goat shit\n" +
+        "well guess what lil boy\n" +
+        "You are..\n" +
+        "\nI Love you Lil Man");
+    Console.WriteLine($"\nUsername: {name}\nPassword: {password}\n");
+    
+}
+
+
+
+
 Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine("Press Enter to move on..\n");
 
 
 Console.ReadKey();
 Console.ForegroundColor = ConsoleColor.White;
-Console.WriteLine($"Now we are going to be messing with...");
+AnsiConsole.MarkupLine("\n[bold]Next were going to be talking about...[/]"); 
 var progressbar_title = new Rule("[red]PROGRESS BARS[/]");
 progressbar_title.Justification = Justify.Center;
 AnsiConsole.Write(progressbar_title);
+AnsiConsole.MarkupLine("Progress bars are a very aesthetic way of showing\n" +
+    "well.. [red]Progress[/]. It can be edited in many ways and even included other things\n" +
+    "Which arent shown in our example.");
 
 
 // Asynchronous
@@ -51,16 +70,20 @@ await AnsiConsole.Progress()
     .StartAsync(async ctx =>
     {
         // Define tasks
-        var task1 = ctx.AddTask("[green]Reticulating splines[/]");
-        var task2 = ctx.AddTask("[green]Folding space[/]");
+        var task1 = ctx.AddTask("[green]Breaking Backs[/]");
+        var task2 = ctx.AddTask("[green]Folding the space time continium[/]");
+        var task3 = ctx.AddTask("[red]Crazy statements are my Forte[/]");
 
         while (!ctx.IsFinished)
         {
             // Simulate some work
-            await Task.Delay(4300);
+            await Task.Delay(2500);
+       
 
             // Increment
             task1.Increment(9.7);
             task2.Increment(7.3);
+            task3.Increment(17);
+  
         }
     });
