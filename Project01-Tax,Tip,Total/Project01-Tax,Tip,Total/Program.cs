dcc1e962f304ce8f6bd6cb6803bb4b642adf00df
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-01-22 21:18:03 -0500

    Created for loop, allows it to run 5 times and stops

--- Project01-Tax,Tip,Total/Project01-Tax,Tip,Total/Program.cs
+++ Project01-Tax,Tip,Total/Project01-Tax,Tip,Total/Program.cs
@@ -10,33 +10,34 @@
 // Titile
 Console.WriteLine("Project 01 - Tax|Tip|Total");
 Console.WriteLine();
-while (true)
+for (int i = 0; i <= 4; i++)
 {
-double price = 0;
+    // Needed Variables
+    double tax = 0;
+    double tip = 0;
+    double total = 0;
+    double price = 0;
 
-//Prompt aswell as variable assignment
-Console.Write("Enter the Total Amount of your purchase: ");
-price = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine();
+    //Prompt aswell as variable assignment
+    Console.Write("Enter the Total Amount of your purchase: ");
+    price = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine();
 
-// Needed Variables
-double tax = 0;
-double tip = 0;
-double total = 0;
 
-// Color so it looks nice
-// Assigns the proper math to the proper Variable
-// Concantates at the bottom x3
-Console.ForegroundColor = ConsoleColor.Green;
-tax = (double)(price * 0.15);
-Console.WriteLine($"Tax(15%): ${double.Round(tax,4)}");
+    // Color so it looks nice
+    // Assigns the proper math to the proper Variable
+    // Concantates at the bottom x3
+    Console.ForegroundColor = ConsoleColor.Green;
+    tax = (double)(price * 0.15);
+    Console.WriteLine($"Tax(15%): ${double.Round(tax,4)}");
 
-tip = (double)(price * 0.07);
-Console.WriteLine($"Tip(7%): ${double.Round(tip,4)}");
+    tip = (double)(price * 0.07);
+    Console.WriteLine($"Tip(7%): ${double.Round(tip,4)}");
 
-total = (double)(price + tax + tip);
-Console.WriteLine($"Total: ${double.Round(total, 4)}");
-Console.WriteLine();
-Console.ForegroundColor = ConsoleColor.White;    
+    total = (double)(price + tax + tip);
+    Console.WriteLine($"Total: ${double.Round(total, 4)}");
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.White;  
 }
-// User price Variable
+
+     
