b55eb0eeb290483a7d5b416b0995218d3333e477
Your Name <hernandezyoenis@gmail.com>
2024-01-23 19:05:50 -0500

    Fixing cSharp submission problems Fixed DAILYRATEUSINGOPTIONAl
    TO DO:
    
    Math Operators

--- Chapter 3/DailyRate Using Optional Parameters/DailyRate/Program.cs
+++ Chapter 3/DailyRate Using Optional Parameters/DailyRate/Program.cs
@@ -11,32 +11,33 @@ namespace DailyRate
 
         void run()
         {
-            public void run()
-            {
-                double fee = calculateFee(theDailyRate: 375.0);
-                Console.WriteLine("Fee is {fee}");
-            }
-
-            private double calculateFee(double theDailyRate = 500.0, intnoOfDays = 1)
-            {
-                Console.WriteLine("calculateFee using two parameters");
-                return theDailyRate * noOfDays;
-            }
-
-            private double calculateFee(double dailyRate = 500.0)
-            {
-                Console.WriteLine("calculateFee using one parameter");
-                int defaultNoOfDays = 1;
-                return dailyRate * defaultNoOfDays;
-            }
-            private double calculateFee()
-            {
-                Console.WriteLine("calculateFee using hardcoded values");
-                double defaultDailyRate = 400.0;
-                int defaultNoOfDays = 1;
-                return defaultDailyRate * defaultNoOfDays;
-            }
+            double fee = calculateFee(theDailyRate: 375.0);
+            Console.WriteLine($"fee is {fee}");
         }
+
+
+        private double calculateFee(double theDailyRate = 500.0, int noOfDays = 1)
+        {
+            Console.WriteLine("Calculate using two optional Parameters");
+            return theDailyRate * noOfDays;
+        }
+
+
+        private double calculateFee(double dailyRate = 500.0)
+        {
+            Console.WriteLine("Calculate using one optional Parameters");
+            int defaultNoofDays = 1;
+            return dailyRate * defaultNoofDays;
+        }
+
+
+        private double calculateFee()
+        {
+            Console.WriteLine("Calculate using Hardcoded Values");
+            double defaultDailyRate = 400.0;
+            int defaultNoOfDays = 1;
+            return defaultDailyRate * defaultNoOfDays;
+
         }
     }
-
+}
