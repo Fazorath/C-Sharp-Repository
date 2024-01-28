f6a508b1e8135cb1e008f414d29f1724b68c9265
Exempt <Hernandezyoenis@gmail.com>
2024-01-28 16:02:22 -0500

    Chapter 4 files Transferred

---
+++ Chapter 4/Selection/Selection/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Selection
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.firstDate.SelectedDate = DateTime.Today;
+            this.secondDate.SelectedDate = DateTime.Today;
+        }
+
+        private void compareClick(object sender, RoutedEventArgs e)
+        {
+            int diff = dateCompare(firstDate.Date.LocalDateTime, secondDate.Date.LocalDateTime);
+            info.Text = "";
+            show("firstDate == secondDate", diff == 0);
+            show("firstDate != secondDate", diff != 0);
+            show("firstDate <  secondDate", diff < 0);
+            show("firstDate <= secondDate", diff <= 0);
+            show("firstDate >  secondDate", diff > 0);
+            show("firstDate >= secondDate", diff >= 0);
+        }
+
+        private void show(string exp, bool result)
+        {
+            info.Text += exp;
+            info.Text += " : " + result.ToString();
+            info.Text += "\n";
+        }
+
+        private int dateCompare(DateTime leftHandSide, DateTime rightHandSide)
+        {
+            // TO DO
+            return 42;
+        }
+    }
+}
---
+++ Chapter 4/SwitchStatement using Pattern Matching - Complete/SwitchStatement/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SwitchStatement
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void copyClick(object sender, RoutedEventArgs e)
+        {
+            target.Text = "";
+            string from = source.Text;
+            for (int i = 0; i != from.Length; i++)
+            {
+                char current = from[i];
+                copyOne(current);
+            }
+        }
+
+        private void copyOne(char current)
+        {
+            target.Text += current switch
+            {
+                '<' => "&lt;",
+                '>' => "&gt;",
+                '&' => "&amp;",
+                '\"' => "&#34;",
+                '\'' => "&#39;",
+                _ => current
+            };
+        }
+    }
+}
