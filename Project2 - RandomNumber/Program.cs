922a88238191d096e057c8cc39f380d31b2f055c
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-02-05 01:25:12 -0500

    Getting ready for project 2. Random number and attempt counter lol doesnt seem to hard want to make it look very pretty.

---
+++ Project2 - RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿// Yoenis Hernandez 
+// Started 2/4/24
+// Project 2 Random Number Generator
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Welcome to my Random Number Guesser");
+Console.ForegroundColor = ConsoleColor.White;
+/* TO DO
+ * Establish random number seed 1 - 100
+ * Ask the user to guess what the number is and create the logic for
+ * If the guess was high display -> Too high try again 
+ * If the guess was low display -> Too Low Try again
+ * While also keeping count of how many attempts
+ * if user correctly guesses
+ * Display attempts and congratulate
+ * then ask to play again 
+ */
