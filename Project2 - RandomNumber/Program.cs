1969a011b8873f5c06bb43cdd2bc7d00f142805e
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-02-05 22:10:15 -0500

    basically finished my project 2 lol. Still missing some QOL that i have to do but basically done in a fit of Locked the fuck in

--- Project2 - RandomNumber/Program.cs
+++ Project2 - RandomNumber/Program.cs
@@ -1,16 +1,77 @@
 ﻿// Yoenis Hernandez 
 // Started 2/4/24
+// Finished 2/6/24
 // Project 2 Random Number Generator
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("Welcome to my Random Number Guesser");
-Console.ForegroundColor = ConsoleColor.White;
-/* TO DO
- * Establish random number seed 1 - 100
- * Ask the user to guess what the number is and create the logic for
- * If the guess was high display -> Too high try again 
- * If the guess was low display -> Too Low Try again
- * While also keeping count of how many attempts
- * if user correctly guesses
- * Display attempts and congratulate
- * then ask to play again 
- */
+
+
+// Functions that run the madness
+Title();
+CheckAnswer();
+
+
+
+void Title()
+    //Title Function
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Welcome to my Random Number Guesser\n");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+int randomnumber()
+    // Returns a random number between 1 and 100 using the Random Built in Class.
+{
+   Random rnd = new Random(); // Random number as shown in book
+    int randnum = rnd.Next(0, 100); // initialized into int variable
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("\nHere is your random Number: ** ");// returns random integers >= 0 and 100
+    //Console.WriteLine(randnum);
+   return randnum;
+}
+int userguess()
+{
+    int guess = 0; // User guess
+    Console.Write("\nPlease guess what the number generated is: ");
+    guess = Convert.ToInt32(Console.ReadLine()); // Converting string to int
+    return guess;
+
+}
+void CheckAnswer()
+{
+    bool play = true;
+    while (play)
+    {
+        int number = randomnumber(); // new random number
+        int attempts = 0; // clean attempts
+
+        while (true)
+        {
+            int answer = userguess(); // user guesses
+            attempts++;
+
+            if (answer == number) // if right
+            {
+                Console.WriteLine("Congratulations you got the correct Answer\n\n" +
+                                  "It took you: " + attempts + " Attempt(s)\n" +
+                                  "------------------------------------------------");
+                break;
+            }
+            else if (answer > number) // if too high
+            {
+                Console.WriteLine("Too High. Try Again");
+            }
+            else // if too low
+            {
+                Console.WriteLine("Too Low. Try Again");
+            }
+        }
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Would you like to play again? (Y/N): ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        string playAgain = Console.ReadLine().ToUpper(); // added upper to make sure input is standard across
+
+        if (playAgain != "Y")
+        {
+            play = false;
+        }
+    }
+}
