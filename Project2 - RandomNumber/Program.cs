1825edc80e31c05b7bb7fc204bef5979f666c7ac
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-02-06 23:21:31 -0500

    Refactored Project 2 StartGuesser() Function and added some fun stuff at the bottom. Should e ready for submission if i dont find something cool to add.

--- Project2 - RandomNumber/Program.cs
+++ Project2 - RandomNumber/Program.cs
@@ -6,71 +6,87 @@
 
 // Functions that run the madness
 Title();
-CheckAnswer();
+StartGuesser();
 
 
 
 void Title()
-    //Title Function
+//Title Function
 {
     Console.ForegroundColor = ConsoleColor.Red;
     Console.Write("Welcome to my Random Number Guesser\n");
     Console.ForegroundColor = ConsoleColor.White;
 }
 int randomnumber()
-    // Returns a random number between 1 and 100 using the Random Built in Class.
+// Returns a random number between 1 and 100 using the Random Built in Class.
 {
-   Random rnd = new Random(); // Random number as shown in book
+    Random rnd = new Random(); // Random number as shown in book
     int randnum = rnd.Next(0, 100); // initialized into int variable
     Console.ForegroundColor = ConsoleColor.Green;
     Console.Write("\nHere is your random Number: ** ");// returns random integers >= 0 and 100
-    //Console.WriteLine(randnum);
-   return randnum;
+                                                       //Console.WriteLine(randnum);
+    return randnum;
 }
 int userguess()
 {
     int guess = 0; // User guess
     Console.Write("\nPlease guess what the number generated is: ");
-    guess = Convert.ToInt32(Console.ReadLine()); // Converting string to int
+
+
+    guess = int.Parse(Console.ReadLine()); // Converting string to int
     return guess;
 
 }
-void CheckAnswer()
+void PlayoneRound()
+{
+
+    int number = randomnumber(); // new random number
+    int attempts = 0; // clean attempts
+
+    while (true)
+    {
+        int answer = userguess(); // user guesses            attempts++;
+
+        if (answer == number) // if right
+        {
+            Console.WriteLine("Congratulations you got the correct Answer\n\n" +
+                              "It took you: " + attempts + " Attempt(s)\n" +
+                              "------------------------------------------------");
+            break;
+        }
+        else if (answer > number) // if too high
+        {
+            attempts++;
+            Console.WriteLine("Too High. Try Again");
+        }
+        else // if too low
+        {
+            attempts++;
+            Console.WriteLine("Too Low. Try Again");
+        }
+    }
+}
+void StartGuesser()
 {
     bool play = true;
     while (play)
     {
-        int number = randomnumber(); // new random number
-        int attempts = 0; // clean attempts
+        PlayoneRound();
 
-        while (true)
-        {
-            int answer = userguess(); // user guesses
-            attempts++;
-
-            if (answer == number) // if right
-            {
-                Console.WriteLine("Congratulations you got the correct Answer\n\n" +
-                                  "It took you: " + attempts + " Attempt(s)\n" +
-                                  "------------------------------------------------");
-                break;
-            }
-            else if (answer > number) // if too high
-            {
-                Console.WriteLine("Too High. Try Again");
-            }
-            else // if too low
-            {
-                Console.WriteLine("Too Low. Try Again");
-            }
-        }
         Console.ForegroundColor = ConsoleColor.Red;
         Console.Write("Would you like to play again? (Y/N): ");
         Console.ForegroundColor = ConsoleColor.Red;
-        string playAgain = Console.ReadLine().ToUpper(); // added upper to make sure input is standard across
+        string playAgain = Console.ReadLine().ToUpper();
+        Console.Write("------------------------------------------------\n");
 
         if (playAgain != "Y")
         {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\n01010010 01101001 01100011 01101011 00100000 01110010 01101111 01101100 01101100 01100101 01100100\n" +
+                "00100000 01101101 01100001 01111001 01100010 01100101 00101110 ");
+            Thread.Sleep(3000); // Messing around with this so included it.
+            Console.Clear();
+            Console.ForegroundColor= ConsoleColor.White;
             play = false;
         }
     }
