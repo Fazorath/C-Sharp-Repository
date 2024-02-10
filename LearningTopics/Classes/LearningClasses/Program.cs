70286d32c7b30608404c8a4e252014ab32148e02
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-02-10 18:00:52 -0500

    Test cat class.

--- LearningTopics/Classes/LearningClasses/Program.cs
+++ LearningTopics/Classes/LearningClasses/Program.cs
@@ -2,4 +2,20 @@
 Console.WriteLine("Learning Classes\n" +
     "----------------------------------------------");
 
-// You use the class keyword to define a new one
+Console.WriteLine("You use the class keyword to define a new one\n\n");
+
+Cat cat01 = new Cat();
+cat01.name = "Exempt";
+cat01.age = 1;
+cat01.meow();
+
+class Cat
+{
+    public string name;
+    public int age;
+    public void meow()
+    {
+        Console.WriteLine($"Cat name: {name}\n" +
+            $"Cat age: {age}");
+    }
+}
