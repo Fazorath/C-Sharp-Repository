ef7affa4cb8a449dac3b6c7dc37d6eb27aa11307
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-02-10 17:54:32 -0500

    Getting started with the new C# hw. Chapter 7-8-9. Starting with classes.

---
+++ LearningTopics/Classes/LearningClasses/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Learning Classes\n" +
+    "----------------------------------------------");
+
+// You use the class keyword to define a new one
