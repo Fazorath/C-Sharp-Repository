3c8b685df61d2b55a0dd3eba47e2ded6580d9668
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-02-18 22:34:36 -0500

    Comparing Behavriour of struct and class.

--- Chapter 9/StructsAndEnums/StructsAndEnums/Date.cs
+++ Chapter 9/StructsAndEnums/StructsAndEnums/Date.cs
@@ -2,7 +2,7 @@
 
 namespace StructsAndEnums
 {
-    struct Date
+    class Date
     {
         private int year;
         private Month month;
@@ -18,6 +18,16 @@ namespace StructsAndEnums
         {
             string data = $"{this.month} {this.day + 1} {this.year+ 1900}";
             return data;
+        }
+
+        public void AdvanceMonth()
+        {
+            this.month++;
+            if (this.month == Month.December + 1)
+            {
+                this.month = Month.January;
+                this.year++;
+            }
         }
     }
 }
--- Chapter 9/StructsAndEnums/StructsAndEnums/Program.cs
+++ Chapter 9/StructsAndEnums/StructsAndEnums/Program.cs
@@ -11,11 +11,17 @@ namespace StructsAndEnums
             Console.WriteLine(first);
             first++;
             Console.WriteLine(first);
-            */
+        
             var defaultDate = new Date();
             Console.WriteLine(defaultDate);
+            */
             Date weddingAnniversary = new Date(2021, Month.July, 4);
             Console.WriteLine(weddingAnniversary);
+            Date weddingAnniversaryCopy = weddingAnniversary;
+            Console.WriteLine($"Value of copy is { weddingAnniversaryCopy}");
+            weddingAnniversary.AdvanceMonth();
+            Console.WriteLine($"New value of weddingAnniversary is { weddingAnniversary}");
+            Console.WriteLine($"Value of copy is still { weddingAnniversaryCopy}");
         }
 
         static void Main()
