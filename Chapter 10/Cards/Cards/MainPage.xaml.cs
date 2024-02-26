1a24b8790d107fb50945213d266ffd64693573eb
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-02-25 22:10:41 -0500

    Chapter 10 using arrays

---
+++ Chapter 10/Cards/Cards/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Cards
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public const int NumHands = 4;
+        private Pack pack = null;
+        private Hand[] hands = new Hand[NumHands];
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void dealClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // TODO: deal the cards
+            }
+            catch (Exception ex)
+            {
+                MessageDialog msg = new MessageDialog(ex.Message, "Error");
+                _ = msg.ShowAsync();
+            }
+        }
+    }
+}
---
+++ Exam2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 30.
+int x = 7;
+
+int y = 14;
+
+var val = (x < y) && ((x * 2) == y);
+Console.WriteLine(val);
+*/
+
+int x = 5;
+
+x += 5;
+
+Console.WriteLine(x);
+void DoWhile()
+{
+    int i = 0;
+    do
+    {
+        Console.WriteLine(i);
+        i++;
+    } while (i < 10);
+}
+
+DoWhile();
