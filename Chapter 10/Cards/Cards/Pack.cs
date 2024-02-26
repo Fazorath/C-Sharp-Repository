4022e1170a291a0c62b8d67c0c3ea984e3b86617
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-02-25 22:30:56 -0500

    Finished with first part it hink

--- Chapter 10/Cards/Cards/Pack.cs
+++ Chapter 10/Cards/Cards/Pack.cs
@@ -11,25 +11,50 @@ namespace Cards
 
         public Pack()
         {
-            // TODO: initialize the pack of cards
+            this.cardPack = new PlayingCard[NumSuits, CardsPerSuit];
+            for (var suit = Suit.Clubs; suit <= Suit.Spades; suit++)
+            {
+                for (var value = Value.Two; value <= Value.Ace; value++)
+                {
+                    this.cardPack[(int)suit, (int)value] = new
+                    PlayingCard(suit, value);
+                }
+            }
         }
 
         public PlayingCard DealCardFromPack()
         {
-            // TODO: pick a random card, remove it from the pack, and return it
-            throw new NotImplementedException("DealCardFromPack - TBD");
+            var suit = (Suit)randomCardSelector.Next(NumSuits);
+            while (this.IsSuitEmpty(suit))
+            {
+                suit = (Suit)randomCardSelector.Next(NumSuits);
+            }
+            var value = (Value)randomCardSelector.Next(CardsPerSuit);
+            while (this.IsCardAlreadyDealt(suit, value))
+            {
+                value = (Value)randomCardSelector.Next(CardsPerSuit);
+            }
+            var card = this.cardPack[(int)suit, (int)value];
+            this.cardPack[(int)suit, (int)value] = null;
+            return card;
         }
 
         private bool IsSuitEmpty(Suit suit)
         {
-            // TODO: return true if there are no more cards available in this suit
-            throw new NotImplementedException("IsSuitEmpty - TBD");
+            bool result = true;
+            for (var value = Value.Two; value <= Value.Ace; value++)
+            {
+                if (!IsCardAlreadyDealt(suit, value))
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+
         }
 
-        private bool IsCardAlreadyDealt(Suit suit, Value value)
-        {
-            // TODO: return true if this card has already been dealt   
-            throw new NotImplementedException("IsCardAlreadyDealt - TBD");
-        }
+        private bool IsCardAlreadyDealt(Suit suit, Value value) => (this.cardPack[(int)suit, (int)value] == null);
+
     }
 }
