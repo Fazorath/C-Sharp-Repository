be353c7c1611cd35d0c6f7ced1cdc27ec52ca5a8
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-03-24 20:56:52 -0400

    new stuff

---
+++ Chapter 12/Vehicles/Vehicles/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    internal class Vehicle
+    {
+        public void StartEngine(string noiseToMakeWhenStarting)
+        {
+            Console.WriteLine($"Starting engine:{noiseToMakeWhenStarting}");
+        }
+        public void StopEngine(string noiseToMakeWhenStopping)
+        {
+            Console.WriteLine($"Stopping engine:{noiseToMakeWhenStopping}");
+        }
+        public virtual void Drive()
+        {
+            Console.WriteLine("Default implementation of the Drivemethod");
+        }
+    }
+}
+
+
+    
+    
---
+++ Chapter 13/Drawing/Drawing/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+using System.Threading.Tasks;
+
+namespace Drawing
+{
+    class Circle : IDraw, IColor
+    {
+        private int diameter;
+        private int locX = 0, locY = 0;
+        private Ellipse circle = null;
+        public Circle(int diameter)
+        {
+            this.diameter = diameter;
+        }
+        void IDraw.SetLocation(int xCoord, int yCoord)
+        {
+            this.locX = xCoord;
+            this.locY = yCoord;
+        }
+        void IDraw.Draw(Canvas canvas)
+        {
+            this.circle = new Ellipse();
+            this.circle.Height = this.diameter;
+            this.circle.Width = this.diameter;
+            Canvas.SetTop(this.circle, this.locY);
+            Canvas.SetLeft(this.circle, this.locX);
+            canvas.Children.Add(this.circle);
+        }
+        void IColor.SetColor(Color color)
+        {
+            if (this.circle is not null)
+            {
+                SolidColorBrush brush = new SolidColorBrush(color);
+                this.circle.Fill = brush;
+            }
+        }
+
+
+
+    }
+
+}
