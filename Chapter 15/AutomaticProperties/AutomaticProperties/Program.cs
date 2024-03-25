e5534fd9b4f58fb6f668e2bf7793f3f26d388dec
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-03-24 20:56:52 -0400

    Drawing book help new stuff

---
+++ Chapter 15/AutomaticProperties/AutomaticProperties/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AutomaticProperties
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // TODO:
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Chapter 15/StudentEnrolment/StudentEnrolment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StudentEnrolment
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // TODO:
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
