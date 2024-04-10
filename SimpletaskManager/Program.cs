4ce25c239689bfa8b12517637ff01bfe2fc7b9ad
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-04-10 00:02:17 -0400

    Skeleton of the task class and the menu have been built for the new project.

---
+++ SimpletaskManager/Program.cs
@@ -0,0 +1,29 @@
+﻿using SimpleTaskManager;
+using System.Runtime.CompilerServices;
+using static System.Console;
+CustomTask task1 = new CustomTask("Task 1", "Description for Task 1", TimeSpan.FromHours(2), false);
+void menu()
+{
+    bool exit = false;
+    while (exit != true)
+    {
+        WriteLine("""
+            ░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░▀█▀░█▀▀░█░█░▀█▀░█▄█░█▀█░█▀█░█▀█░█▀▀░█▀▀░█▀▄
+            ░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░█░░█▀▀░▄▀▄░░█░░█░█░█▀█░█░█░█▀█░█░█░█▀▀░█▀▄
+            ░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░▀░░▀░░▀░▀░▀░▀░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░▀
+
+            Menu Items
+            1. Add a New Task
+            2.List All Tasks
+            3.Mark a task as Completed
+            4.Delete a Task
+            5.Exit the Application
+            """);
+        int choice = 0;
+        Write("\nEnter your Choice: ");
+        string userInput = ReadLine();
+        
+    }
+}
+
+menu();
---
+++ SimpletaskManager/Task.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTaskManager
+{
+    internal class CustomTask
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TimeSpan Duration { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public CustomTask(string title, string description, TimeSpan duration, bool isCompleted)
+        {
+            Title = title;
+            Description = description;
+            Duration = duration;
+            IsCompleted = isCompleted;
+        }
+
+
+    }
+}
