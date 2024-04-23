ae0f37243ed63428285047acb259a7ef17dd7b0a
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-04-23 18:54:28 -0400

    # Project 7 Started Created the student class with its needed propertes and a constructor.

---
+++ Project7-StudentManagement/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project7_StudentManagement
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Student(int studentId, string firstName, string lastName, int age)
+        {
+            StudentId = studentId;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
