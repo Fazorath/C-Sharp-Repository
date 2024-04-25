6655ea9c82fa22e2207118dc661ee4114d40c53e
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-04-24 22:42:03 -0400

    Messing with lambda expressions before the project and added the list of populated students to use.

---
+++ LearningTopics/LambdaExpressions/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// C# program to illustrate the
+// Lambda Expression
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+            // List to store numbers
+            List<int> numbers = new List<int>() {36, 71, 12,
+                             15, 29, 18, 27, 17, 9, 34};
+
+            // foreach loop to display the list
+            Console.Write("The list : ");
+            foreach (var value in numbers)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
+            // Using lambda expression
+            // to calculate square of
+            // each value in the list
+            var square = numbers.Select(x => x * x);
+
+            // foreach loop to display squares
+            Console.Write("Squares : ");
+            foreach (var value in square)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
+            // Using Lambda expression to
+            // find all numbers in the list
+            // divisible by 3
+            List<int> divBy3 = numbers.FindAll(x => (x % 3) == 0);
+
+            // foreach loop to display divBy3
+            Console.Write("Numbers Divisible by 3 : ");
+            foreach (var value in divBy3)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
--- Project7-StudentManagement/Program.cs
+++ Project7-StudentManagement/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project7_StudentManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of Student objects
+            List<Student> students = new List<Student>
+            {
+                new Student(1, "Yoenis", "Hernandez", 20),
+                new Student(2, "Yannah", "Blaise", 22),
+                new Student(4, "Brandon", "Goat", 21),
+                new Student(5, "Hello", "Mister", 21),
+                new Student(6, "World", "March", 21),
+                new Student(7, "Eggs", "Benedict", 21),
+                // Add more students as needed
+            };
+        }
+    }
+}
