c3495a65369d6fea860cc382081c56e6dd738e4f
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-04-25 00:00:46 -0400

    Moved functions to other file to make it look sexy and professional. still missing docstrings and maybe error input.

--- Project7-StudentManagement/Program.cs
+++ Project7-StudentManagement/Program.cs
@@ -63,24 +63,24 @@ namespace Project7_StudentManagement
                     {
                         case "1":
                             Clear();
-                            var AddedStudent = GetStudentDetails();
-                            AddStudent(students, AddedStudent);
+                            var AddedStudent = StudentFunctions.GetStudentDetails();
+                            StudentFunctions.AddStudent(students, AddedStudent);
                             break;
                         case "2":
                             Clear();
-                            allStudents(students);
+                            StudentFunctions.allStudents(students);
                             break;
                         case "3":
                             Clear();
-                            DisplayStudentsOrderedByFirstName(students);
+                            StudentFunctions.DisplayStudentsOrderedByFirstName(students);
                             break;
                         case "4":
                             Clear();
-                            DisplayStudentOrderedByLastName(students);
+                            StudentFunctions.DisplayStudentOrderedByLastName(students);
                             break;
                         case "5":
                             Clear();
-                            DisplayStudentOrderedByAge(students);   
+                            StudentFunctions.DisplayStudentOrderedByAge(students);   
                             break;
                         case "6":
                             Clear();
@@ -94,181 +94,7 @@ namespace Project7_StudentManagement
                     }
                 }
             }
-            void allStudents(List<Student> students)
-            {
-                ForegroundColor = ConsoleColor.Magenta;
-                WriteLine("""
-                    
-                     ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
-                    ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
-                    ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
-                    ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
-                    ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
-                    ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
-                    ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
-                      ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
-                        ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
-                    
-                        Retrieving All Students!  
-
-                    """);
-                ForegroundColor = ConsoleColor.Green;
-                foreach (Student student in students)
-                {
-                    WriteLine($"ID: {student.StudentId}, Name: {student.FirstName} {student.LastName}, Age: {student.Age}");
-                }
-                ForegroundColor = ConsoleColor.Magenta;
-                Write("\nPress Enter to move on!");
-                ReadKey();
-                Clear();
-            };
-            // Function to display students ordered by alphabetical first name
-            static void DisplayStudentsOrderedByFirstName(List<Student> students)
-            {
-                ForegroundColor = ConsoleColor.Magenta;
-                WriteLine("""
-                   
-                    ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
-                   ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
-                   ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
-                   ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
-                   ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
-                   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
-                   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
-                     ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
-                       ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
-                   
-                       Retrieving Student First Names
-                       
-                   """);
-                ForegroundColor = ConsoleColor.Green;
-                var studentsOrderedByFirstName = students.OrderBy(s => s.FirstName);
-                foreach (var student in studentsOrderedByFirstName)
-                {
-                    Console.WriteLine($"Name: {student.FirstName} {student.LastName}");
-                }
-                ForegroundColor = ConsoleColor.Magenta;
-                Write("\nPress Enter to move on!");
-                ReadKey();
-                Clear();
-            }
-
-            static void DisplayStudentOrderedByLastName(List<Student> students)
-            {
-                ForegroundColor = ConsoleColor.Magenta;
-                WriteLine("""
-                   
-                    ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
-                   ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
-                   ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
-                   ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
-                   ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
-                   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
-                   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
-                     ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
-                       ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
-                   
-                       Retrieving Student Last Names
-                       
-                   """);
-                ForegroundColor = ConsoleColor.Green;
-                var studentOrderedByLastName = students.OrderBy(s => s.LastName);
-                foreach(var student in studentOrderedByLastName)
-                {
-                    Console.WriteLine($"Name: {student.LastName} {student.FirstName}");
-                }
-                ForegroundColor = ConsoleColor.Magenta;
-                Write("\nPress Enter to move on!");
-                ReadKey();
-                Clear();
-            }
-            static void DisplayStudentOrderedByAge(List<Student> students)
-            {
-                ForegroundColor = ConsoleColor.Magenta;
-                WriteLine("""
-                   
-                    ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
-                   ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
-                   ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
-                   ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
-                   ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
-                   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
-                   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
-                     ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
-                       ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
-                   
-                       Retrieving Students Between the ages of 18 and 30
-                       
-                   """);
-                ForegroundColor = ConsoleColor.Green;
-                var orderedStudents = students
-                                    .Where(s => s.Age >= 18 && s.Age <= 30)
-                .OrderBy(s => s.Age);
-                foreach (var student in orderedStudents)
-                {
-                    Console.WriteLine($"ID: {student.StudentId}, Name: {student.FirstName} {student.LastName}, Age: {student.Age}");
-                }
-                ForegroundColor = ConsoleColor.Magenta;
-                Write("\nPress Enter to move on!");
-                ReadKey();
-                Clear();
-            }
-            // Function to ask for student details and return a Student object
-            static Student GetStudentDetails()
-            {
-                ForegroundColor = ConsoleColor.Magenta;
-                WriteLine("""
-                   
-                    ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
-                   ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
-                   ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
-                   ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
-                   ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
-                   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
-                   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
-                     ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
-                       ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
-                   
-                       Retrieving Students Between the ages of 18 and 30
-                       
-                   """);
-                Console.WriteLine("Enter student details:");
-                Console.Write("Student ID: ");
-                ForegroundColor = ConsoleColor.Green;
-                int studentId = int.Parse(Console.ReadLine());
-
-                ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("First Name: ");
-                ForegroundColor = ConsoleColor.Green;
-                string firstName = Console.ReadLine();
-
-                ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("Last Name: ");
-                ForegroundColor = ConsoleColor.Green;
-                string lastName = Console.ReadLine();
-
-                ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("Age: ");
-                ForegroundColor = ConsoleColor.Green;
-                int age = int.Parse(Console.ReadLine());
-
-                // Create a new Student object with the provided details
-                Student newStudent = new Student(studentId, firstName, lastName, age);
-                return newStudent;
-            }
-
-            // Function to add a student to the list
-            static void AddStudent(List<Student> students, Student student)
-            {
-                // Add the new student to the list
-                students.Add(student);
-                Write($"\nStudent {student.FirstName} {student.LastName} added successfully!");
-                ForegroundColor = ConsoleColor.Magenta;
-                Write("\nPress Enter to move on!");
-                ReadKey();
-                Clear();
-            }
-
+            
             Menu();
         }
     }
---
+++ Project7-StudentManagement/StudentFunctions.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Project7_StudentManagement
+{
+    public class StudentFunctions
+    {
+        public static void allStudents(List<Student> students)
+        {
+            ForegroundColor = ConsoleColor.Magenta;
+            WriteLine("""
+                    
+                     ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
+                    ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
+                    ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
+                    ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
+                    ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
+                    ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
+                    ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
+                      ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
+                        ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
+                    
+                        Retrieving All Students!  
+
+                    """);
+            ForegroundColor = ConsoleColor.Green;
+            foreach (Student student in students)
+            {
+                WriteLine($"ID: {student.StudentId}, Name: {student.FirstName} {student.LastName}, Age: {student.Age}");
+            }
+            ForegroundColor = ConsoleColor.Magenta;
+            Write("\nPress Enter to move on!");
+            ReadKey();
+            Clear();
+        }
+        // Function to display students ordered by alphabetical first name
+        public static void DisplayStudentsOrderedByFirstName(List<Student> students)
+        {
+            ForegroundColor = ConsoleColor.Magenta;
+            WriteLine("""
+                   
+                    ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
+                   ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
+                   ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
+                   ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
+                   ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
+                   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
+                   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
+                     ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
+                       ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
+                   
+                       Retrieving Student First Names
+                       
+                   """);
+            ForegroundColor = ConsoleColor.Green;
+            var studentsOrderedByFirstName = students.OrderBy(s => s.FirstName);
+            foreach (var student in studentsOrderedByFirstName)
+            {
+                Console.WriteLine($"Name: {student.FirstName} {student.LastName}");
+            }
+            ForegroundColor = ConsoleColor.Magenta;
+            Write("\nPress Enter to move on!");
+            ReadKey();
+            Clear();
+        }
+
+        public static void DisplayStudentOrderedByLastName(List<Student> students)
+        {
+            ForegroundColor = ConsoleColor.Magenta;
+            WriteLine("""
+                   
+                    ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
+                   ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
+                   ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
+                   ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
+                   ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
+                   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
+                   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
+                     ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
+                       ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
+                   
+                       Retrieving Student Last Names
+                       
+                   """);
+            ForegroundColor = ConsoleColor.Green;
+            var studentOrderedByLastName = students.OrderBy(s => s.LastName);
+            foreach (var student in studentOrderedByLastName)
+            {
+                Console.WriteLine($"Name: {student.LastName} {student.FirstName}");
+            }
+            ForegroundColor = ConsoleColor.Magenta;
+            Write("\nPress Enter to move on!");
+            ReadKey();
+            Clear();
+        }
+        public static void DisplayStudentOrderedByAge(List<Student> students)
+        {
+            ForegroundColor = ConsoleColor.Magenta;
+            WriteLine("""
+                   
+                    ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
+                   ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
+                   ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
+                   ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
+                   ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
+                   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
+                   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
+                     ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
+                       ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
+                   
+                       Retrieving Students Between the ages of 18 and 30
+                       
+                   """);
+            ForegroundColor = ConsoleColor.Green;
+            var orderedStudents = students
+                                .Where(s => s.Age >= 18 && s.Age <= 30)
+            .OrderBy(s => s.Age);
+            foreach (var student in orderedStudents)
+            {
+                Console.WriteLine($"ID: {student.StudentId}, Name: {student.FirstName} {student.LastName}, Age: {student.Age}");
+            }
+            ForegroundColor = ConsoleColor.Magenta;
+            Write("\nPress Enter to move on!");
+            ReadKey();
+            Clear();
+        }
+        // Function to ask for student details and return a Student object
+        public static Student GetStudentDetails()
+        {
+            ForegroundColor = ConsoleColor.Magenta;
+            WriteLine("""
+                   
+                    ██▓     ██▓ ███▄    █   █████       ██████ ▓██   ██▓  ██████ ▄▄▄█████▓▓█████  ███▄ ▄███▓
+                   ▓██▒    ▓██▒ ██ ▀█   █ ▒██▓  ██▒   ▒██    ▒  ▒██  ██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒
+                   ▒██░    ▒██▒▓██  ▀█ ██▒▒██▒  ██░   ░ ▓██▄     ▒██ ██░░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██    ▓██░
+                   ▒██░    ░██░▓██▒  ▐▌██▒░██  █▀ ░     ▒   ██▒  ░ ▐██▓░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ 
+                   ░██████▒░██░▒██░   ▓██░░▒███▒█▄    ▒██████▒▒  ░ ██▒▓░▒██████▒▒  ▒██▒ ░ ░▒████▒▒██▒   ░██▒
+                   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒▒░ ▒    ▒ ▒▓▒ ▒ ░   ██▒▒▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░
+                   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░ ▒░  ░    ░ ░▒  ░ ░ ▓██ ░▒░ ░ ░▒  ░ ░    ░     ░ ░  ░░  ░      ░
+                     ░ ░    ▒ ░   ░   ░ ░    ░   ░    ░  ░  ░   ▒ ▒ ░░  ░  ░  ░    ░         ░   ░      ░   
+                       ░  ░ ░           ░     ░             ░   ░ ░           ░              ░  ░       ░   
+                   
+                       Retrieving Students Between the ages of 18 and 30
+                       
+                   """);
+            Console.WriteLine("Enter student details:");
+            Console.Write("Student ID: ");
+            ForegroundColor = ConsoleColor.Green;
+            int studentId = int.Parse(Console.ReadLine());
+
+            ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("First Name: ");
+            ForegroundColor = ConsoleColor.Green;
+            string firstName = Console.ReadLine();
+
+            ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Last Name: ");
+            ForegroundColor = ConsoleColor.Green;
+            string lastName = Console.ReadLine();
+
+            ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Age: ");
+            ForegroundColor = ConsoleColor.Green;
+            int age = int.Parse(Console.ReadLine());
+
+            // Create a new Student object with the provided details
+            Student newStudent = new Student(studentId, firstName, lastName, age);
+            return newStudent;
+        }
+
+        // Function to add a student to the list
+        public static void AddStudent(List<Student> students, Student student)
+        {
+            // Add the new student to the list
+            students.Add(student);
+            WriteLine($"Student {student.FirstName} {student.LastName} added successfully!");
+            ForegroundColor = ConsoleColor.Magenta;
+            Write("\nPress Enter to move on!");
+            ReadKey();
+            Clear();
+        }
+    }
+}
