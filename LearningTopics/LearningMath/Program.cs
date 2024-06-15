efc3bd0951a2f14146770a1cd73f25748b376d70
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-06-15 15:29:08 -0400

    different ways to work with the Math. function.

--- LearningTopics/LearningMath/Program.cs
+++ LearningTopics/LearningMath/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double x = 2.4567;
+double y = 5;
+
+//double a = Math.Pow(x, 2);
+//double b = Math.Sqrt(x);
+//double c = Math.Abs(x);
+//double d = Math.Round(x);
+//double e = Math.Ceiling(x);
+//double f = Math.Floor(x);
+//double g = Math.Max(x, y);
+double h = Math.Min(x, y);
+
+Console.WriteLine(h);
