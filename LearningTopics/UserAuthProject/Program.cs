4c65f3ec102bdf00b286443f8f9ec1ff51601442
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-06-14 23:31:58 -0400

    Made a test user and service class to work on the addition and  hashing of the password on the DB

--- LearningTopics/UserAuthProject/Program.cs
+++ LearningTopics/UserAuthProject/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using UserAuthProject.Services;
+
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Register a new user");
+            Console.Write("Enter your email: ");
+            var email = Console.ReadLine();
+
+            Console.Write("Enter your password: ");
+            var password = Console.ReadLine();
+
+            UserService.RegisterUser(email, password);
+
+            Console.WriteLine("User registered successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
