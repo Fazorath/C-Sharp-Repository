e6eb6a3dc265edee78b894edd62c127afdf242c3
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-06-14 23:06:54 -0400

    Working on the UserAuthProject

---
+++ LearningTopics/UserAuthProject/Data/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAuthProject.Data
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=UserAuthDB;User Id=root;Password=root;");
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+
+}
